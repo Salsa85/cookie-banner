96f915240a31e0d254c4c03903f82470bea65d1e
Sasla85 <DLC20142691vs@>
2025-05-09 12:23:32 +0200

    feat: Consent Mode only, remove unused config, set default consent to granted, bump to v0.2.0

--- config/cookie-banner.php
+++ config/cookie-banner.php
@@ -12,9 +12,6 @@ return [
 
     'privacy_policy_url' => env('COOKIE_BANNER_PRIVACY_URL', '/privacy-policy'),
     
-    'gtm_id' => env('COOKIE_BANNER_GTM_ID', ''),
-    'analytics_id' => env('COOKIE_BANNER_ANALYTICS_ID', ''),
-    
     'messages' => [
         'banner_text' => 'Wij gebruiken cookies om uw ervaring op onze website te verbeteren. Door op \'Accepteren\' te klikken gaat u akkoord met het gebruik van alle cookies.',
         'essential_button' => 'Alleen noodzakelijk',
